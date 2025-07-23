ea7c3dd09a53b94d0cd932445359f5316d9d9cc6
Sabith <masterdevsabith@gmail.com>
2025-07-23 17:10:01 +0530

    did some stripe api works

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,21 @@
+import Stripe from "stripe";
+import { NextRequest, NextResponse } from "next/server";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+export async function POST(req: NextRequest, res: NextResponse) {
+  const payload = await req.text();
+  const response = JSON.parse(payload);
+
+  const sig = req.headers.get("Stripe-Signature");
+
+  const dateTime = new Date(response?.created * 1000).toLocaleDateString();
+  const timeString = new Date(response?.created * 1000).toLocaleDateString();
+
+  try {
+    let event = stripe.webhooks.constructEvent(
+      payload,
+      sig!,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error) {}
+}
